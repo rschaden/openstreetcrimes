417d24b8f406bc2bf2ac5cb22001e0274b1e1815
rschaden <rschaden@gmail.com>
2012-11-08 13:05:48 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openstreetcrimes::Application.config.secret_token = 'b457cc5e5e9afee6869e5c827420ac4134d283d2685cce35ef5ec6846151d2421dfa8a2f6254d9f20f362a49806e8a5b48e7e5db33fd85fd92860f14f3f13226'
