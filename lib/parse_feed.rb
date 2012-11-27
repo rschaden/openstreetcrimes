21bf1419e6d8afabd70cf2eac135af5071230c27
rschaden <rschaden@gmail.com>
2012-11-27 18:21:04 +0100

    change parsefeed interface to return single streets/districts

--- lib/parse_feed.rb
+++ lib/parse_feed.rb
@@ -22,18 +22,33 @@ module Osc
                        "platz",
                        "damm"]
 
-    def getDistricts(string)
+    def district(string)
+      districts = get_districts(string)
+      return "" if districts.empty?
+
+      districts.first
+    end
+
+    def street(string)
+      streets = get_streets(string)
+      return "" if streets.empty?
+
+      streets.first
+    end
+
+    private
+    def get_districts(string)
       DISTRICTS.select { |district| string.match(district) }
     end
 
-    def getStreets(string)
+    def get_streets(string)
       return [] if string.nil?
       STREET_SUFFIXES.map do |suffix|
         street = "\\p{Word}*#{suffix}\\b"
         cap_street = street.gsub("#{suffix}", "\\s#{suffix.capitalize}")
         street_regexp = Regexp.new("(#{street}|#{cap_street})")
-        # require 'pry'; binding.pry
-        string.scan(street_regexp)
+          # require 'pry'; binding.pry
+          string.scan(street_regexp)
       end.flatten.reject(&:empty?)
     end
   end
