dfc5c6d66e867943ece8efddf204283237c24a3c
rschaden <rschaden@gmail.com>
2012-11-27 18:40:03 +0100

    add specs for #location_string

--- spec/models/raw_crime_spec.rb
+++ spec/models/raw_crime_spec.rb
@@ -24,8 +24,42 @@ describe RawCrimes do
 
     it 'does not allow duplicate guids' do
       duplicate_crime = FactoryGirl.build(:raw_crimes,
-                                        guid: @raw_crime.guid)
+                                          guid: @raw_crime.guid)
       duplicate_crime.should_not be_valid
+    end
+  end
+
+  context '#location_string' do
+    before :each do
+      @raw_crime = FactoryGirl.create :raw_crimes
+    end
+
+    it 'without a street and district' do
+      Osc::ParseFeed.stub(:street).and_return ''
+      Osc::ParseFeed.stub(:district).and_return ''
+
+      @raw_crime.location_string.should eq 'Berlin'
+    end
+
+    it 'with only a district' do
+      Osc::ParseFeed.stub(:street).and_return ''
+      Osc::ParseFeed.stub(:district).and_return 'Mitte'
+
+      @raw_crime.location_string.should eq 'Mitte Berlin'
+    end
+
+    it 'with only a street' do
+      Osc::ParseFeed.stub(:street).and_return 'Lange Strasse'
+      Osc::ParseFeed.stub(:district).and_return ''
+
+      @raw_crime.location_string.should eq 'Lange Strasse Berlin'
+    end
+
+    it 'with both street and district' do
+      Osc::ParseFeed.stub(:street).and_return 'Lange Strasse'
+      Osc::ParseFeed.stub(:district).and_return 'Mitte'
+
+      @raw_crime.location_string.should eq 'Lange Strasse Mitte Berlin'
     end
   end
 
