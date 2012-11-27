63e90aeadc4d587f1ced725d634ae3bb27f07ad2
rschaden <rschaden@gmail.com>
2012-11-27 18:53:03 +0100

    refactor location_string specs

--- spec/models/raw_crime_spec.rb
+++ spec/models/raw_crime_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+# require 'parse_feed'
 
 describe RawCrimes do
 
@@ -34,32 +35,18 @@ describe RawCrimes do
       @raw_crime = FactoryGirl.create :raw_crimes
     end
 
-    it 'without a street and district' do
-      Osc::ParseFeed.stub(:street).and_return ''
-      Osc::ParseFeed.stub(:district).and_return ''
+    [{ street: '', district: '' },
+     { street: 'Lange Strasse', district: '' },
+     { street: '', district: 'Mitte' },
+     { street: 'Lange Strasse', district: 'Mitte' },
+    ].each do |elem|
+      it "with street='#{elem[:street]}' and district='#{elem[:district]}'" do
+        Osc::ParseFeed.stub(:street).and_return elem[:street]
+        Osc::ParseFeed.stub(:district).and_return elem[:district]
 
-      @raw_crime.location_string.should eq 'Berlin'
-    end
-
-    it 'with only a district' do
-      Osc::ParseFeed.stub(:street).and_return ''
-      Osc::ParseFeed.stub(:district).and_return 'Mitte'
-
-      @raw_crime.location_string.should eq 'Mitte Berlin'
-    end
-
-    it 'with only a street' do
-      Osc::ParseFeed.stub(:street).and_return 'Lange Strasse'
-      Osc::ParseFeed.stub(:district).and_return ''
-
-      @raw_crime.location_string.should eq 'Lange Strasse Berlin'
-    end
-
-    it 'with both street and district' do
-      Osc::ParseFeed.stub(:street).and_return 'Lange Strasse'
-      Osc::ParseFeed.stub(:district).and_return 'Mitte'
-
-      @raw_crime.location_string.should eq 'Lange Strasse Mitte Berlin'
+        expected = "#{elem[:street]} #{elem[:district]} Berlin".squish
+        @raw_crime.location_string.should eq expected
+      end
     end
   end
 
