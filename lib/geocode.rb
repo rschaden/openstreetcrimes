a875a9dc0259fe1a1549ec72c728d2d6115afd28
rschaden <rschaden@gmail.com>
2012-11-27 19:25:48 +0100

    return nil if location not found

--- lib/geocode.rb
+++ lib/geocode.rb
@@ -4,6 +4,8 @@ module Osc
   class Geocode
     def self.get_point(location)
       point = Geocoder.coordinates(location)
+      return nil if point.nil?
+
       factory = RGeo::Geographic.simple_mercator_factory
 
       factory.point(point.second, point.first)
