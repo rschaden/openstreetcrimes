081ece7ff4657e83437e753e8fa0fe90f00d683b
rschaden <rschaden@gmail.com>
2013-01-17 13:25:41 +0100

    add lonlat option for geocoder

--- lib/geocode.rb
+++ lib/geocode.rb
@@ -3,12 +3,16 @@ require 'geocoder'
 module Osc
   class Geocode
     def self.get_point(location)
+      get_lonlat(location).projection
+    end
+
+    def self.get_lonlat(location)
       point = Geocoder.coordinates(location)
       return nil if point.nil?
 
       factory = Osc::GEOFACTORY
 
-      factory.point(point.second, point.first).projection
+      factory.point(point.second, point.first)
     end
   end
 end
