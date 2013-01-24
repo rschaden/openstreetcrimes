20642b65d12647901d4c7ccc37a9a9add760a149
rschaden <rschaden@gmail.com>
2013-01-24 17:25:42 +0100

    make district accessible for crimes

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -2,7 +2,7 @@ class Crime < ActiveRecord::Base
   belongs_to :crime_type
   belongs_to :district
 
-  attr_accessible :date, :description, :location
+  attr_accessible :date, :description, :location, :district
 
   set_rgeo_factory_for_column(:location, Osc::GEOFACTORY.projection_factory)
 end
