cbeb435aaf886aee1bfb7e7e137b4f12ceb2ea04
rschaden <rschaden@gmail.com>
2013-01-26 19:09:31 +0100

    add method to get location of raw_crime

--- lib/geocode.rb
+++ lib/geocode.rb
@@ -15,5 +15,28 @@ module Osc
 
       factory.point(point.second, point.first)
     end
+
+    def self.raw_crime(raw_crime)
+      streets = Osc::ParseFeed.get_streets(raw_crime.text)
+      districts = Osc::ParseFeed.get_streets(raw_crime.title)
+
+      streets.each do |street|
+        districts.each do |district|
+          lonlat = get_point(location_string(street, district))
+          return lonlat if lonlat
+          sleep 1
+        end
+        if districts.empty?
+          lonlat = get_point(location_string(street))
+          return lonlat if lonlat
+          sleep 1
+        end
+      end
+      return nil
+    end
+
+    def self.location_string(street, district = '')
+      "#{street} #{district} Berlin".squish
+    end
   end
 end
