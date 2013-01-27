523067d94d15bf1d66632899c1a01766c0d6a65a
rschaden <rschaden@gmail.com>
2013-01-27 17:46:41 +0100

    refine regexp (match Alt-x)

--- lib/osc/parse_feed.rb
+++ lib/osc/parse_feed.rb
@@ -44,13 +44,14 @@ module Osc
 
     # Awesome Regex: http://rubular.com/r/bVZyPD8tWG
     def get_streets(string)
-      regex_street_signifier_lowercase = STREET_SIGNIFIER.join '|'
-      regex_street_signifier_capitalized = STREET_SIGNIFIER.map(&:capitalize).join '|'
-      regex_street_prefixes = STREET_PREFIXES.join '|'
-      monster_regexp = /(((#{regex_street_prefixes}) )?((#{regex_street_signifier_capitalized}) de[r|s] (([0-9]+\. )?\p{Lu}\p{L}+ ?)+|#{FIX_STREETS.join('|')}|\p{Lu}[\p{L}-]+(#{regex_street_signifier_lowercase})|(\p{L}+-)+(#{regex_street_signifier_capitalized})|\p{Lu}\p{L}+ (#{regex_street_signifier_capitalized}))\b)/
+      street_signifier_lowercase = STREET_SIGNIFIER.join '|'
+      street_signifier_capitalized = STREET_SIGNIFIER.map(&:capitalize).join '|'
+      street_prefixes = STREET_PREFIXES.join '|'
+      monster_regexp = /(((Alt-)\p{Lu}\p{L}+\b|(#{FIX_STREETS.join('|')}))|(((#{street_prefixes}) )?((#{street_signifier_capitalized}) de[r|s] (([0-9]+\. )?\p{Lu}\p{L}+ ?)+|\p{Lu}[\p{L}-]+(#{street_signifier_lowercase})|(\p{L}+-)+(#{street_signifier_capitalized})|\p{Lu}\p{L}+ (#{street_signifier_capitalized}))\b))/
+
       matches = string.scan monster_regexp
       return [] if matches.nil?
-      matches.uniq.map(&:first).reject{ |m| m =~ /Gehweg|Richtung|(Vor|Park)platz/ }
+      matches.uniq.map(&:first).reject{ |m| m =~ /Gehweg|Richtung|(Vor|Park)platz|Elektronikmarkt/ }
     end
 
     def get_districts(string)
