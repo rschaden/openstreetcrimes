8a3b6a1939011950fc62f3ef491cf39aac3a8e93
rschaden <rschaden@gmail.com>
2013-01-27 11:41:05 +0100

    validations for crime.guid

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -2,7 +2,9 @@ class Crime < ActiveRecord::Base
   belongs_to :crime_type
   belongs_to :district
 
-  attr_accessible :date, :description, :location, :district
+  attr_accessible :guid, :date, :description, :location, :district
+
+  validates :guid, presence: true, uniqueness: true
 
   set_rgeo_factory_for_column(:location, Osc::GEOFACTORY.projection_factory)
 end
