8fc221602a4650a065be84d9511034e0d4dd54c9
rschaden <rschaden@gmail.com>
2013-01-28 19:18:24 +0100

    add historic counts to district

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,5 +1,6 @@
 class District < ActiveRecord::Base
   has_many :crimes
+  has_many :crime_histories
 
   attr_accessible :name, :area, :population
 
@@ -11,5 +12,15 @@ class District < ActiveRecord::Base
 
   def weighted_crime_count
     crime_count * 100000.0 / population
+  end
+
+  def historic_count(year)
+    year = CrimeHistory.where(district_id: id).where(year: year)
+    return year.first.crime_count unless year.empty?
+    0
+  end
+
+  def weighted_historic_count(year)
+    historic_count(year) * 100000.0 / population
   end
 end
