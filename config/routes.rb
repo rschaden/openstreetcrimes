1f57e3e0b295fe59ef5e93c2fb75e28cb8683295
Hendrik Bergunde <hbergunde@gmx.de>
2013-01-29 11:01:29 +0100

    make default url / accessable

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Openstreetcrimes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'maps#district_heatmap'
 
   # See how all your routes lay out with "rake routes"
 
